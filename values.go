4e602a1cb46f071dbc606bbb2a462bb31b4fb43f
Boanerget <antonyprogramacion96@gmail.com>
2017-09-14 13:08:22 -0400

    [add] values

---
+++ values.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//concatenations
+	fmt.Println("gith" + "ub")
+
+	//whole and floating
+
+	fmt.Println("1+1 =", 1 + 1)
+
+	fmt.Println("7.0/3.0 =" , 7.0/3.0 )
+
+	//booleans
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+}
