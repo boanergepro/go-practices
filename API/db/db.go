4f59835ad040c388f963f0f6a7a2a849db895763
Boanerget <antonyprogramacion96@gmail.com>
2017-09-28 17:30:14 -0400

    [add][mod] Directorio creado para la configuracion de la base de datos.

---
+++ API/db/db.go
@@ -0,0 +1,18 @@
+package db
+
+import (
+	
+	arango "github.com/diegogub/aranGO"
+
+)
+
+func GetSessionDB() *arango.Session {
+
+	//Connection ArangoDB
+	s, err := arango.Connect("http://192.168.0.100:8529","boanergepro", "123456", false)
+
+	if err != nil {
+		panic(err)
+	}
+	return s
+}
