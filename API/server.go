9f7adf49514dfce0d1dcfb7b2887fbb151e4c607
Boanerget <antonyprogramacion96@gmail.com>
2017-09-28 10:36:22 -0400

    [mod] todos los handler se han separado en diferentes funciones.

--- API/server.go
+++ API/server.go
@@ -11,12 +11,27 @@ import (
 
 type Usuario struct {
 	arango.Document //Hay que incluirlo siempre en todas las estructuras porque añade campos nativos de arango
-	Usuario     string `json:"usuario"`
-	Password    string `json:"password"`
+	Usuario   string `json:"usuario"`
+	Password  string `json:"password"`
 	Email 	  string `json:"email"`
 }
 
+//Connect(host, user, password string, log bool) (*Session, error)
+var session, err = arango.Connect("http://192.168.0.100:8529","boanergepro","123456",false)
+
 func main() {
+
+	if err != nil{
+		panic(err)
+	}
+
+	//Creando una colleccion si no existe
+	if !session.DB("boanergepro").ColExist("usuarios") {
+
+		documento := arango.NewCollectionOptions("usuarios", true)
+		session.DB("boanergepro").CreateCollection(documento)
+
+	}
 
 	app := iris.New()
 
@@ -27,149 +42,31 @@ func main() {
 			AllowedHeaders:   []string{"*"},
 			AllowCredentials: true,
 			Debug:            true,
-		}))
-	
-	//Connect(host, user, password string, log bool) (*Session, error) {
-    session,err := arango.Connect("http://192.168.0.100:8529","boanergepro","123456",false) 
-    if err != nil{
-        panic(err)
-    }
-
-    //Creando una colleccion si no existe
-    if !session.DB("boanergepro").ColExist("usuarios") {
-
-    	documento := arango.NewCollectionOptions("usuarios", true)
-    	session.DB("boanergepro").CreateCollection(documento)
-
-    }
+	}))
 
     //Index del API
-    app.Get("/api", HandlerIndex)
+    app.Get("/api", HandleIndex)
    
     //CREAR
-	app.Post("/api/usuarios", func (contexto context.Context) {
-		var user Usuario
-		contexto.ReadJSON(&user)
-
-    	//Insertar documento
-    	err = session.DB("boanergepro").Col("usuarios").Save(&user)
-    	if err != nil {
-    		contexto.StatusCode(iris.StatusInternalServerError)
-    	}
-
-		contexto.StatusCode(iris.StatusOK)
-	})
-
-	//VER TODOS
-	app.Get("/api/usuarios", func(contexto context.Context) {
-		
-		query := arango.NewQuery(` 
-			FOR usuario in usuarios
-			RETURN usuario
-		`) 
-
-		cursor, err := session.DB("boanergepro").Execute(query)
-		if  err != nil {
-    		contexto.StatusCode(iris.StatusInternalServerError)
-    	}
-
-    	var resultados []Usuario
-
-    	cursor.FetchBatch(&resultados)
-    	contexto.StatusCode(iris.StatusOK)
-		contexto.JSON(resultados)
-		
-	})
+	app.Post("/api/usuarios", HandlerCreateUser)
 
 	//VER
-	app.Get("/api/usuarios/{key:string}", func(contexto context.Context) {
-		
-		key_params := contexto.Params().Get("key")
+	app.Get("/api/usuarios/{key:string}", HandlerUser)
 
-		query := arango.NewQuery(` 
-			FOR usuario in usuarios
-			FILTER usuario._key == @key
-			RETURN usuario
-		`)
-		query.BindVars = map[string]interface{}{
-			"key": key_params,
-		}
-
-		cursor, err := session.DB("boanergepro").Execute(query)
-		if  err != nil {
-			contexto.StatusCode(iris.StatusInternalServerError)
-    	}
-
-    	var resultado Usuario
-
-    	cursor.FetchOne(&resultado)
-    	contexto.StatusCode(iris.StatusOK)
-		contexto.JSON(resultado)
-		
-	})
+	//VER TODOS
+	app.Get("/api/usuarios", HandlerAllUsers)
 	
 	//ACTUALIZAR
-	app.Put("/api/usuarios/{key:string}", func(contexto context.Context) {
-
-		key_params := contexto.Params().Get("key")
-
-		query := arango.NewQuery(` 
-			FOR usuario in usuarios
-			FILTER usuario._key == @key
-			RETURN usuario
-		`)
-		query.BindVars = map[string]interface{}{
-			"key": key_params,
-		}
-
-		cursor, err := session.DB("boanergepro").Execute(query)
-
-	    if err != nil {
-	    	contexto.StatusCode(iris.StatusInternalServerError)
-	    }
-
-	    var user_actualiar Usuario
-
-    	cursor.FetchOne(&user_actualiar)
-
-    	if contexto.PostValue("usuario") != "" {
-    		user_actualiar.Usuario = contexto.PostValue("usuario")
-    	}
-    	if contexto.PostValue("password") != "" {
-    		user_actualiar.Password = contexto.PostValue("password")
-    	}
-    	if contexto.PostValue("email") != "" {
-    		user_actualiar.Email = contexto.PostValue("email")
-    	}
-
-    	err = session.DB("boanergepro").Col("usuarios").Replace(user_actualiar.Key, user_actualiar)
-    	if err != nil {
-    		contexto.StatusCode(iris.StatusInternalServerError)
-    	}
-
-    	contexto.StatusCode(iris.StatusOK)
-	})
+	app.Put("/api/usuarios/{key:string}", HandlerUpdateUser)
 	
 	//ELIMINAR
-	app.Delete("/api/usuarios/{key:string}", func(contexto context.Context) {
-
-		key_params := contexto.Params().Get("key")
-
-		err = session.DB("boanergepro").Col("usuarios").Delete(key_params)
-
-	    if err != nil {
-	    	contexto.StatusCode(iris.StatusInternalServerError)
-	    }
-
-	    contexto.StatusCode(iris.StatusOK)
-
-	})
+	app.Delete("/api/usuarios/{key:string}",HandlerDeleteUser)
 
     //Servidor corriendo en http://localhost:8080
     app.Run(iris.Addr(":8080"))
 }
 
-func HandlerIndex (contexto context.Context) {
+func HandleIndex (contexto context.Context) {
 	contexto.HTML(`
 		<h1>Api corriendo</h1><br>
 		<h4>Recursos:</h4><br>
@@ -180,4 +77,113 @@ func HandlerIndex (contexto context.Context) {
 			</li>
 		</ul>
     `)
+}
+
+func HandlerCreateUser (contexto context.Context) {
+	var user Usuario
+	contexto.ReadJSON(&user)
+
+	//Insertar documento
+	err = session.DB("boanergepro").Col("usuarios").Save(&user)
+	if err != nil {
+		contexto.StatusCode(iris.StatusInternalServerError)
+	}
+
+	contexto.StatusCode(iris.StatusOK)
+}
+
+func HandlerAllUsers (contexto context.Context) {
+
+	query := arango.NewQuery(`
+			FOR usuario in usuarios
+			RETURN usuario
+		`)
+
+	cursor, err := session.DB("boanergepro").Execute(query)
+	if  err != nil {
+		contexto.StatusCode(iris.StatusInternalServerError)
+	}
+
+	var resultados []Usuario
+
+	cursor.FetchBatch(&resultados)
+	contexto.StatusCode(iris.StatusOK)
+	contexto.JSON(resultados)
+
+}
+
+func HandlerUser (contexto context.Context) {
+	key_params := contexto.Params().Get("key")
+
+	query := arango.NewQuery(`
+			FOR usuario in usuarios
+			FILTER usuario._key == @key
+			RETURN usuario
+		`)
+	query.BindVars = map[string]interface{}{
+		"key": key_params,
+	}
+
+	cursor, err := session.DB("boanergepro").Execute(query)
+	if  err != nil {
+		contexto.StatusCode(iris.StatusInternalServerError)
+	}
+
+	var resultado Usuario
+
+	cursor.FetchOne(&resultado)
+	contexto.StatusCode(iris.StatusOK)
+	contexto.JSON(resultado)
+}
+
+func HandlerUpdateUser (contexto context.Context) {
+	key_params := contexto.Params().Get("key")
+
+	query := arango.NewQuery(`
+			FOR usuario in usuarios
+			FILTER usuario._key == @key
+			RETURN usuario
+		`)
+	query.BindVars = map[string]interface{}{
+		"key": key_params,
+	}
+
+	cursor, err := session.DB("boanergepro").Execute(query)
+
+	if err != nil {
+		contexto.StatusCode(iris.StatusInternalServerError)
+	}
+
+	var user_actualiar Usuario
+
+	cursor.FetchOne(&user_actualiar)
+
+	if contexto.PostValue("usuario") != "" {
+		user_actualiar.Usuario = contexto.PostValue("usuario")
+	}
+	if contexto.PostValue("password") != "" {
+		user_actualiar.Password = contexto.PostValue("password")
+	}
+	if contexto.PostValue("email") != "" {
+		user_actualiar.Email = contexto.PostValue("email")
+	}
+
+	err = session.DB("boanergepro").Col("usuarios").Replace(user_actualiar.Key, user_actualiar)
+	if err != nil {
+		contexto.StatusCode(iris.StatusInternalServerError)
+	}
+
+	contexto.StatusCode(iris.StatusOK)
+}
+
+func HandlerDeleteUser (contexto context.Context) {
+	key_params := contexto.Params().Get("key")
+
+	err = session.DB("boanergepro").Col("usuarios").Delete(key_params)
+
+	if err != nil {
+		contexto.StatusCode(iris.StatusInternalServerError)
+	}
+
+	contexto.StatusCode(iris.StatusOK)
 }
