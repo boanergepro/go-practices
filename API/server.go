2772a8906bb31fffce1a6a72db2d146debd1f16d
Boanerget <antonyprogramacion96@gmail.com>
2017-09-29 15:05:42 -0400

    [mod] Todos los handlers fueron movidos a otros controladores.

--- API/server.go
+++ API/server.go
@@ -35,117 +35,19 @@ func main() {
    
     //CREAR
 	app.Post("/api/usuarios", controladores.HandlerCreateUser)
-	/*
+	
 	//VER
-	app.Get("/api/usuarios/{key:string}", HandlerUser)
+	app.Get("/api/usuarios/{key:string}", controladores.HandlerUser)
 
 	//VER TODOS
-	app.Get("/api/usuarios", HandlerAllUsers)
+	app.Get("/api/usuarios", controladores.HandlerAllUsers)
 	
 	//ACTUALIZAR
-	app.Put("/api/usuarios/{key:string}", HandlerUpdateUser)
-	
+	app.Put("/api/usuarios/{key:string}", controladores.HandlerUpdateUser)
+
 	//ELIMINAR
-	app.Delete("/api/usuarios/{key:string}",HandlerDeleteUser)
-	*/
+	app.Delete("/api/usuarios/{key:string}", controladores.HandlerDeleteUser)
+
     //Servidor corriendo en http://localhost:8080
     app.Run(iris.Addr(":8080"))
 }
-
-/*
-func HandlerAllUsers (contexto context.Context) {
-
-	query := arango.NewQuery(`
-			FOR usuario in usuarios
-			RETURN usuario
-		`)
-
-	cursor, err := session.DB("boanergepro").Execute(query)
-	if  err != nil {
-		contexto.StatusCode(iris.StatusInternalServerError)
-	}
-
-	var resultados []Usuario
-
-	cursor.FetchBatch(&resultados)
-	contexto.StatusCode(iris.StatusOK)
-	contexto.JSON(resultados)
-
-}
-
-func HandlerUser (contexto context.Context) {
-	key_params := contexto.Params().Get("key")
-
-	query := arango.NewQuery(`
-			FOR usuario in usuarios
-			FILTER usuario._key == @key
-			RETURN usuario
-		`)
-	query.BindVars = map[string]interface{}{
-		"key": key_params,
-	}
-
-	cursor, err := session.DB("boanergepro").Execute(query)
-	if  err != nil {
-		contexto.StatusCode(iris.StatusInternalServerError)
-	}
-
-	var resultado Usuario
-
-	cursor.FetchOne(&resultado)
-	contexto.StatusCode(iris.StatusOK)
-	contexto.JSON(resultado)
-}
-
-func HandlerUpdateUser (contexto context.Context) {
-	key_params := contexto.Params().Get("key")
-
-	query := arango.NewQuery(`
-			FOR usuario in usuarios
-			FILTER usuario._key == @key
-			RETURN usuario
-		`)
-	query.BindVars = map[string]interface{}{
-		"key": key_params,
-	}
-
-	cursor, err := session.DB("boanergepro").Execute(query)
-
-	if err != nil {
-		contexto.StatusCode(iris.StatusInternalServerError)
-	}
-
-	var user_actualiar Usuario
-
-	cursor.FetchOne(&user_actualiar)
-
-	if contexto.PostValue("usuario") != "" {
-		user_actualiar.Usuario = contexto.PostValue("usuario")
-	}
-	if contexto.PostValue("password") != "" {
-		user_actualiar.Password = contexto.PostValue("password")
-	}
-	if contexto.PostValue("email") != "" {
-		user_actualiar.Email = contexto.PostValue("email")
-	}
-
-	err = session.DB("boanergepro").Col("usuarios").Replace(user_actualiar.Key, user_actualiar)
-	if err != nil {
-		contexto.StatusCode(iris.StatusInternalServerError)
-	}
-
-	contexto.StatusCode(iris.StatusOK)
-}
-
-func HandlerDeleteUser (contexto context.Context) {
-	key_params := contexto.Params().Get("key")
-
-	err = session.DB("boanergepro").Col("usuarios").Delete(key_params)
-
-	if err != nil {
-		contexto.StatusCode(iris.StatusInternalServerError)
-	}
-
-	contexto.StatusCode(iris.StatusOK)
-}
-*/
